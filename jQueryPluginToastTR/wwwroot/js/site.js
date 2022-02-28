1a85f54a79c621a351928f458577ce63b592ba93
Nikunj018 <delvadiyanikunj1234@gmail.com>
2022-02-28 16:27:29 +0530

    Add project files.

---
+++ jQueryPluginToastTR/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*
+
+$(document).ready(function () {
+
+    alert("test SiteJS")
+    toastr.error('Error', 'Error Title')
+    toastr["success"]("My name is Inigo Montoya. You killed my father. Prepare to die!")
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-left",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "3000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+});
+            
+          */ 
